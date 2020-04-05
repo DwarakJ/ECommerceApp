d05b298ba7d6ceb7f500118bbb5fba66b6cdd8d2
MANIARASAN SIVASERAN <arasuseran16@gmail.com>
2020-04-05 20:05:39 +0530

    Initial commit - watersense

---
+++ api-service/src/index.ts
@@ -0,0 +1,16 @@
+import {WatersenseApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {WatersenseApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new WatersenseApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
